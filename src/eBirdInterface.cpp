8ddda67b84a360df678803ebda42a5b0bf784ff9
Kerry <kerryloux@gmail.com>
2017-08-23 09:27:31 -0400

    Started building framework for interfacing with eBird API

---
+++ src/eBirdInterface.cpp
@@ -0,0 +1,8 @@
+// File:  eBirdInterface.cpp
+// Date:  8/22/2017
+// Auth:  K. Loux
+// Desc:  Interface to eBird web API.
+
+// Local headers
+#include "eBirdInterface.h"
+
---
+++ src/eBirdInterface.h
@@ -0,0 +1,15 @@
+// File:  eBirdInterface.h
+// Date:  8/22/2017
+// Auth:  K. Loux
+// Desc:  Interface to eBird web API.
+
+#ifndef EBIRD_INTERFACE_H_
+
+class EBirdInterface
+{
+public:
+
+private:
+};
+
+#endif// EBIRD_INTERFACE_H_
