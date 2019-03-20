9312a4353d3582f3ed9e232e36ebeee27a4fd1bd
Kerry <kerryloux@gmail.com>
2019-03-20 13:40:34 -0400

    Cleaned up use of config in main application class

--- src/eBirdDataProcessorApp.cpp
+++ src/eBirdDataProcessorApp.cpp
@@ -41,121 +41,123 @@ int EBirdDataProcessorApp::Run(int argc, char *argv[])
 	if (!configFile.ReadConfiguration(configFileName))
 		return 1;
 
-	if (!configFile.GetConfig().eBirdDatasetPath.empty())
+	const auto& config(configFile.GetConfig());
+
+	if (!config.eBirdDatasetPath.empty())
 	{
 		EBirdDatasetInterface dataset;
-		if (!configFile.GetConfig().timeOfDayOutputFile.empty())
+		if (!config.timeOfDayOutputFile.empty())
 		{
-			EBirdInterface ebi(configFile.GetConfig().eBirdApiKey);
-			const auto regionCode(ebi.GetRegionCode(configFile.GetConfig().countryFilter,
-				configFile.GetConfig().stateFilter, configFile.GetConfig().countyFilter));
-			if (!dataset.ExtractTimeOfDayInfo(configFile.GetConfig().eBirdDatasetPath,
-				configFile.GetConfig().timeOfDataCommonNames, regionCode, configFile.GetConfig().splitRegionDataFile))
+			EBirdInterface ebi(config.eBirdApiKey);
+			const auto regionCode(ebi.GetRegionCode(config.countryFilter,
+				config.stateFilter, config.countyFilter));
+			if (!dataset.ExtractTimeOfDayInfo(config.eBirdDatasetPath,
+				config.timeOfDataCommonNames, regionCode, config.splitRegionDataFile))
 				return 1;
-			if (!dataset.WriteTimeOfDayFiles(configFile.GetConfig().timeOfDayOutputFile, EBirdDatasetInterface::TimeOfDayPeriod::Week))
+			if (!dataset.WriteTimeOfDayFiles(config.timeOfDayOutputFile, EBirdDatasetInterface::TimeOfDayPeriod::Week))
 				return 1;
 		}
 		else// Ignore all other options and generate global frequency data
 		{
-			if (!dataset.ExtractGlobalFrequencyData(configFile.GetConfig().eBirdDatasetPath, configFile.GetConfig().splitRegionDataFile))
+			if (!dataset.ExtractGlobalFrequencyData(config.eBirdDatasetPath, config.splitRegionDataFile))
 				return 1;
-			if (dataset.WriteFrequencyFiles(configFile.GetConfig().frequencyFilePath))
+			if (dataset.WriteFrequencyFiles(config.frequencyFilePath))
 				return 1;
 		}
 		return 0;
 	}
 
 	EBirdDataProcessor processor;
-	if (!processor.Parse(configFile.GetConfig().dataFileName))
+	if (!processor.Parse(config.dataFileName))
 		return 1;
 
-	if (configFile.GetConfig().uniqueObservations != EBDPConfig::UniquenessType::None)
-		processor.GenerateUniqueObservationsReport(configFile.GetConfig().uniqueObservations);
+	if (config.uniqueObservations != EBDPConfig::UniquenessType::None)
+		processor.GenerateUniqueObservationsReport(config.uniqueObservations);
 
 	// Remove entires that don't fall withing the specified locations
-	if (!configFile.GetConfig().locationFilter.empty() && configFile.GetConfig().targetNeedArea == EBDPConfig::TargetNeedArea::None)
-		processor.FilterLocation(configFile.GetConfig().locationFilter, configFile.GetConfig().countyFilter,
-			configFile.GetConfig().stateFilter, configFile.GetConfig().countryFilter);
-	else if (!configFile.GetConfig().countyFilter.empty() && configFile.GetConfig().targetNeedArea <= EBDPConfig::TargetNeedArea::Region)
-		processor.FilterCounty(configFile.GetConfig().countyFilter, configFile.GetConfig().stateFilter,
-			configFile.GetConfig().countryFilter);
-	else if (!configFile.GetConfig().stateFilter.empty() && configFile.GetConfig().targetNeedArea <= EBDPConfig::TargetNeedArea::Subnational1)
-		processor.FilterState(configFile.GetConfig().stateFilter, configFile.GetConfig().countryFilter);
-	else if (!configFile.GetConfig().countryFilter.empty() && configFile.GetConfig().targetNeedArea <= EBDPConfig::TargetNeedArea::Country)
-		processor.FilterCountry(configFile.GetConfig().countryFilter);
+	if (!config.locationFilter.empty() && config.targetNeedArea == EBDPConfig::TargetNeedArea::None)
+		processor.FilterLocation(config.locationFilter, config.countyFilter,
+			config.stateFilter, config.countryFilter);
+	else if (!config.countyFilter.empty() && config.targetNeedArea <= EBDPConfig::TargetNeedArea::Region)
+		processor.FilterCounty(config.countyFilter, config.stateFilter,
+			config.countryFilter);
+	else if (!config.stateFilter.empty() && config.targetNeedArea <= EBDPConfig::TargetNeedArea::Subnational1)
+		processor.FilterState(config.stateFilter, config.countryFilter);
+	else if (!config.countryFilter.empty() && config.targetNeedArea <= EBDPConfig::TargetNeedArea::Country)
+		processor.FilterCountry(config.countryFilter);
 
 	// Remove entries that don't fall within the specified dates
-	if (configFile.GetConfig().yearFilter > 0)
-		processor.FilterYear(configFile.GetConfig().yearFilter);
+	if (config.yearFilter > 0)
+		processor.FilterYear(config.yearFilter);
 
-	if (configFile.GetConfig().monthFilter > 0)
-		processor.FilterMonth(configFile.GetConfig().monthFilter);
+	if (config.monthFilter > 0)
+		processor.FilterMonth(config.monthFilter);
 
-	if (configFile.GetConfig().weekFilter > 0)
-		processor.FilterWeek(configFile.GetConfig().weekFilter);
+	if (config.weekFilter > 0)
+		processor.FilterWeek(config.weekFilter);
 
-	if (configFile.GetConfig().dayFilter > 0)
-		processor.FilterDay(configFile.GetConfig().dayFilter);
+	if (config.dayFilter > 0)
+		processor.FilterDay(config.dayFilter);
 		
 	// Remove entries that don't contain the specified comment string
-	if (!configFile.GetConfig().commentGroupString.empty())
-		processor.FilterCommentString(configFile.GetConfig().commentGroupString);
+	if (!config.commentGroupString.empty())
+		processor.FilterCommentString(config.commentGroupString);
 
 	// Other filter criteria
-	if (!configFile.GetConfig().includePartialIDs)
+	if (!config.includePartialIDs)
 		processor.FilterPartialIDs();
 
-	if (!configFile.GetConfig().mediaListHTML.empty())
+	if (!config.mediaListHTML.empty())
 	{
-		processor.GenerateMediaList(configFile.GetConfig().mediaListHTML, configFile.GetConfig().mediaFileName);
+		processor.GenerateMediaList(config.mediaListHTML, config.mediaFileName);
 		return 0;
 	}
-	else if (!configFile.GetConfig().mediaFileName.empty())
-		processor.ReadMediaList(configFile.GetConfig().mediaFileName);
+	else if (!config.mediaFileName.empty())
+		processor.ReadMediaList(config.mediaFileName);
 
 	// TODO:  species count only?
 
-	if (configFile.GetConfig().generateRarityScores)
-		processor.GenerateRarityScores(configFile.GetConfig().frequencyFilePath,
-			configFile.GetConfig().listType, configFile.GetConfig().eBirdApiKey,
-			configFile.GetConfig().countryFilter, configFile.GetConfig().stateFilter,
-			configFile.GetConfig().countyFilter);
-	else if (configFile.GetConfig().findMaxNeedsLocations)
+	if (config.generateRarityScores)
+		processor.GenerateRarityScores(config.frequencyFilePath,
+			config.listType, config.eBirdApiKey,
+			config.countryFilter, config.stateFilter,
+			config.countyFilter);
+	else if (config.findMaxNeedsLocations)
 	{
-		EBirdInterface ebi(configFile.GetConfig().eBirdApiKey);
-		const auto regionCode(ebi.GetRegionCode(configFile.GetConfig().countryFilter,
-			configFile.GetConfig().stateFilter, configFile.GetConfig().countyFilter));
+		EBirdInterface ebi(config.eBirdApiKey);
+		const auto regionCode(ebi.GetRegionCode(config.countryFilter,
+			config.stateFilter, config.countyFilter));
 		if (!processor.FindBestLocationsForNeededSpecies(
-			configFile.GetConfig().frequencyFilePath, configFile.GetConfig().kmlLibraryPath,
-			configFile.GetConfig().eBirdApiKey, regionCode,
-			configFile.GetConfig().highDetailCountries, configFile.GetConfig().cleanupKMLLocationNames))
+			config.frequencyFilePath, config.kmlLibraryPath,
+			config.eBirdApiKey, regionCode,
+			config.highDetailCountries, config.cleanupKMLLocationNames))
 			return 1;
 	}
-    else if (configFile.GetConfig().generateTargetCalendar)
+    else if (config.generateTargetCalendar)
 	{
-		processor.GenerateTargetCalendar(configFile.GetConfig().topBirdCount,
-			configFile.GetConfig().outputFileName, configFile.GetConfig().frequencyFilePath,
-			configFile.GetConfig().countryFilter, configFile.GetConfig().stateFilter, configFile.GetConfig().countyFilter,
-			configFile.GetConfig().recentObservationPeriod,
-			configFile.GetConfig().targetInfoFileName, configFile.GetConfig().homeLocation,
-			configFile.GetConfig().googleMapsAPIKey, configFile.GetConfig().eBirdApiKey);
+		processor.GenerateTargetCalendar(config.topBirdCount,
+			config.outputFileName, config.frequencyFilePath,
+			config.countryFilter, config.stateFilter, config.countyFilter,
+			config.recentObservationPeriod,
+			config.targetInfoFileName, config.homeLocation,
+			config.googleMapsAPIKey, config.eBirdApiKey);
 	}
-	else if (configFile.GetConfig().doComparison)
+	else if (config.doComparison)
 		processor.DoListComparison();
 	else
 	{
-		processor.SortData(configFile.GetConfig().primarySort, configFile.GetConfig().secondarySort);
+		processor.SortData(config.primarySort, config.secondarySort);
 
-		const UString::String list(processor.GenerateList(configFile.GetConfig().listType,
-			configFile.GetConfig().showOnlyPhotoNeeds, configFile.GetConfig().showOnlyAudioNeeds));
+		const UString::String list(processor.GenerateList(config.listType,
+			config.showOnlyPhotoNeeds, config.showOnlyAudioNeeds));
 		Cout << list << std::endl;
 
-		if (!configFile.GetConfig().outputFileName.empty())
+		if (!config.outputFileName.empty())
 		{
-			UString::OFStream outFile(configFile.GetConfig().outputFileName.c_str());
+			UString::OFStream outFile(config.outputFileName.c_str());
 			if (!outFile.is_open() || !outFile.good())
 			{
-				Cerr << "Failed to open '" << configFile.GetConfig().outputFileName << "' for output\n";
+				Cerr << "Failed to open '" << config.outputFileName << "' for output\n";
 				return 1;
 			}
 
