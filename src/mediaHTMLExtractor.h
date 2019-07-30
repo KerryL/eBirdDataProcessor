b7f950d4acbf80d3bbc02c1acb8d220f71d45d95
Kerry <kerryloux@gmail.com>
2019-07-30 10:26:52 -0400

    Working on automatic media list generation

---
+++ src/mediaHTMLExtractor.h
@@ -0,0 +1,82 @@
+// File:  mediaHTMLExtractor.h
+// Date:  7/18/2019
+// Auth:  K. Loux
+// Desc:  Object for interfacing with ebird.org web site, navigating to
+//        user's media profile and extracting the relevant portion of the HTML.
+
+#ifndef MEDIA_HTML_EXTRACTOR_H_
+#define MEDIA_HTML_EXTRACTOR_H_
+
+// Local headers
+#include "email/jsonInterface.h"
+#include "throttledSection.h"
+#include "processPipe.h"
+
+// Local forward declarations
+class WebSocketWrapper;
+
+class MediaHTMLExtractor : JSONInterface
+{
+public:
+	MediaHTMLExtractor();
+	~MediaHTMLExtractor() = default;
+
+	bool ExtractMediaHTML(const UString::String& htmlFileName);
+
+private:
+	static const bool verbose;
+	static const UString::String userAgent;
+	static const std::string eBirdLoginURL;
+	static const std::string eBirdProfileURL;
+	static const std::string baseURL;
+
+	ThrottledSection rateLimiter;
+
+	ProcessPipe browserPipe;
+	unsigned int commandId = 0;
+
+	std::string BuildNavigateCommand(const std::string& url);
+	std::string BuildEnableDOMCommand();
+	std::string BuildGetDocumentNodeCommand();
+	std::string BuildGetFullDocumentNodeCommand();
+	std::string BuildGetHTMLCommand();
+	std::string BuildKeyInputCommand(const char& c, const std::string& type, const std::string& payloadFieldName);
+	std::string BuildSetFocusCommand(const int& nodeID);
+
+	bool GetCurrentHTML(WebSocketWrapper& ws, std::string& html);
+	bool SimulateTextEntry(WebSocketWrapper& ws, const std::string& s);
+	bool SimulateRawKey(WebSocketWrapper& ws, const std::string& s);
+	bool SendKeyEvents(WebSocketWrapper& ws, const std::string& s, const std::string& type, const std::string& payloadFieldName);
+	typedef std::vector<std::pair<std::string, std::string>> AttributeVector;
+	bool FocusOnElement(WebSocketWrapper& ws, cJSON* nodesArray, const std::string& nodeName, const AttributeVector& attributes);
+
+	cJSON* GetDOMNodesArray(WebSocketWrapper& ws, cJSON*& root);
+
+	bool LaunchBrowser();
+	bool DoEBirdLogin(WebSocketWrapper& ws);
+
+	static bool EBirdLoginSuccessful(const std::string& htmlData);
+	static void GetUserNameAndPassword(std::string& userName, std::string& password);
+
+	static bool ExtractTextBetweenTags(const std::string& htmlData, const std::string& startTag,
+		const std::string& endTag, std::string& text, std::string::size_type& offset);
+	static std::string ExtractTokenFromLoginPage(const std::string& htmlData);
+
+	static std::string ModifyMediaListLink(const std::string& link);
+	static std::string RemoveParameter(const std::string& link, const std::string& parameter);
+	static bool ExtractTextContainedInTag(const std::string& htmlData,
+		const std::string& startTag, std::string& text);
+
+	bool GetMediaListHTML(std::string& mediaListHTML);
+	static std::string FindMediaListURL(const std::string& profileHTML);
+
+	static bool ExtractWebSocketURL(const std::string& json, UString::String& webSocketURL);
+	static bool ExtractNodeID(const std::string& json, int& nodeID);
+	static bool ExtractResult(const std::string& json, std::string& result);
+
+	static std::chrono::steady_clock::duration GetCrawlDelay();
+
+	static bool ResponseHasError(const std::string& response);
+};
+
+#endif// MEDIA_HTML_EXTRACTOR_H_
